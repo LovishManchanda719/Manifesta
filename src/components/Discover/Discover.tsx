42ad16439b175447768445981ebd6112fd297c09
AditiSingh-09 <154231889+AditiSingh-09@users.noreply.github.com>
2024-11-02 15:25:53 +0530

    Update Discover.tsx

--- src/components/Discover/Discover.tsx
+++ src/components/Discover/Discover.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import "./discover.css"
-import Card from "./Card.tsx"
+import Card from "./Card.jsx"
 
 
 
 const Discover = () => {
   return (
     <div className="max-w-[1200px] mx-auto">
-        <div className="header min-w-[320px]">Uncover your interest.</div>
-        <div className="flex flex-wrap justify-center mt-8">
+        <div className="flex font-['Roboto_Condensed',_sans-serif] font-extrabold text-[55px] h-[60px] mt-16 mb-[70px]  justify-center items-center min-w-[500px]">Uncover your interest.</div>
+        <div className="flex flex-wrap justify-center items-center mt-8">
             <Card title="Kalakriti" image="src/assets/kalakriti.jpeg" />
             <Card title="Sahitya" image="src/assets/sahitya.jpeg" />
             <Card title="UAS" image="src/assets/uas.jpeg" />
